c8ddee7619a8c8fee96df753d0ca385dce9088c6
kalle söderlund <jens.karsod08@edu.umea.se>
2026-09-03 13:48:04 +0200

    hhh

--- .vscode/ridm.py
+++ .vscode/ridm.py
@@ -1,73 +1,87 @@
 import random
+x = 10
+
+def gambling(return_prize,bank2):
+    # print("\033c", end="")
 
 
-def gambling(return_prize, du,bank2,x):
+
     prize = 100
     if return_prize != 0:
         prize = return_prize
     print ("priset liger på", (prize))
 
-
+    
     fråga = input("vil du riska att alt för att få dubelt? ") 
+    
 
 
     if fråga == "nej":
         print ("du van", prize)
-        bank_func(prize,bank2,x)
+        bank_func(prize,bank2,bank05)
 
-    elif fråga == "ja":
+    else:
         opp = random.randint(1, 10)
         du = random.randint(1, x)
-        
+       #print("din tur är", (x))
+
+ 
+
 
         if (opp > du):
-            print ("you lost")
+            print ("du  förlora")
             prize = 100
 
         elif (opp < du):
-            print ("you win")
+            print ("du van")
             prize = (prize * 2)
             print (prize)
+        
+
+        gambling(prize,bank2)
 
 
-        gambling(prize,du,bank2,x)
-
-
-def bank_func(prize,bank2,x):
+def bank_func(prize,bank2,bank05):
     
-    bank = 0
-    bank2 = (prize + bank)
+    bank = prize
+
+    bank05 = bank2
+
+    bank2 = (bank05 + bank)
 
     print ("du har", bank2)
     igen = input ("vill du riska mera pengar? ")
 
     if igen == "ja":
-        gambling(0,du,bank2,x)
+        gambling(0,bank2)
 
     elif igen == "nej":
-        store(du,bank2,x)
+        store(bank2, bank05)
 
 
-def store(du,bank2,x):
-
+def store(bank2, bank05):
+    
+    global x
     luck = input ("vil du sköpa mera tur för 300???")   
 
 
     if luck == "ja":
         if bank2 > 300:
-            x * 1000000000000000
-            gambling(0,du,bank2,x)
+            x = x * 1,3
+            bank2 = bank2 - 300
+            gambling(0,bank2)
 
         elif bank2 < 300:
             print ("brokie")
-            gambling(0,du,bank2,x)
+            gambling(0,bank2)
 
     elif luck == "nej":
             print ("okej")
-            gambling(0,du,bank2,x)
+            gambling(0,bank2)
 
-x = 10
+bank2 = 0
+bank05 = 0
 bank2 = 0
 du = random.randint(1, 10)
-gambling(0, du, bank2,x)
+gambling(0,bank2)
 
