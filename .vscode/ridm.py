b0eb343d7f723d5c47762edf7c9966c3f867409c
kalle söderlund <jens.karsod08@edu.umea.se>
2026-09-03 12:56:50 +0200

    lektion random!

--- .vscode/ridm.py
+++ .vscode/ridm.py
@@ -1,7 +1,7 @@
 import random
 
 
-def gambling(return_prize):
+def gambling(return_prize, du,bank2,x):
     prize = 100
     if return_prize != 0:
         prize = return_prize
@@ -13,13 +13,12 @@ def gambling(return_prize):
 
     if fråga == "nej":
         print ("du van", prize)
-        bank_func(prize)
-
-
+        bank_func(prize,bank2,x)
 
     elif fråga == "ja":
         opp = random.randint(1, 10)
-        du = random.randint(1, 10)
+        du = random.randint(1, x)
+        
 
         if (opp > du):
             print ("you lost")
@@ -30,10 +29,11 @@ def gambling(return_prize):
             prize = (prize * 2)
             print (prize)
 
-        gambling(prize)
+
+        gambling(prize,du,bank2,x)
 
 
-def bank_func(prize):
+def bank_func(prize,bank2,x):
     
     bank = 0
     bank2 = (prize + bank)
@@ -42,18 +42,32 @@ def bank_func(prize):
     igen = input ("vill du riska mera pengar? ")
 
     if igen == "ja":
-        gambling(0)
+        gambling(0,du,bank2,x)
 
     elif igen == "nej":
-        store(bank2)
+        store(du,bank2,x)
 
 
-def store(bank2):
+def store(du,bank2,x):
 
     luck = input ("vil du sköpa mera tur för 300???")   
 
-    if luck == "ja":
-        gambling(0)
 
-gambling(0)
+    if luck == "ja":
+        if bank2 > 300:
+            x * 1000000000000000
+            gambling(0,du,bank2,x)
+
+        elif bank2 < 300:
+            print ("brokie")
+            gambling(0,du,bank2,x)
+
+    elif luck == "nej":
+            print ("okej")
+            gambling(0,du,bank2,x)
+
+x = 10
+bank2 = 0
+du = random.randint(1, 10)
+gambling(0, du, bank2,x)
 
